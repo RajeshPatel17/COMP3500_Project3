fe5194788c511af0bad365cb3e174ce904c9d282
Rajesh R Patel <rrp0019@tux063.eng.auburn.edu>
2021-10-14 20:13:24 -0500

    wrote catsem, not compiled

--- kern/asst1/catsem.c
+++ kern/asst1/catsem.c
@@ -44,6 +44,27 @@
 
 #define NMICE 2
 
+static struct semaphore *mutex;
+static struct semaphore *cats_queue;
+static struct semaphore *mice_queue;
+static struct semaphore *dish_mutex;
+static struct semaphore *cat_done;
+static struct semaphore *mouse_done;
+
+int dish1_busy = 0;
+int dish2_busy = 0;
+int no_cat_eat = 1;
+int all_dishes_avail = 1;
+int first_cat_eat = 0;
+int another_cat_eat = 0;
+int first_mouse_eat = 0;
+int another_mouse_eat = 0;
+int no_mouse_eat = 1;
+
+
+int cat_wait_count = 0;
+int mice_wait_count = 0;
+
 
 /*
  * 
@@ -52,6 +73,187 @@
  */
 
 
+static void FirstCatNoMouse(int catNum){
+        P(mutex);
+        kprintf("\nCat %d is hungry", catNum);
+        if(all_dishes_avail){
+                all_dishes_avail = 0;
+                V(cats_queue);
+        }
+        cat_wait_count++;
+        V(mutex);
+        P(cats_queue);
+        if(no_cat_eat){
+                no_cat_eat = 0;
+                first_cat_eat = 1;
+        } else {
+                first_cat_eat = 0;
+        }
+}
+
+static void FirstCat(int catNum){
+
+        if(first_cat_eat){
+                P(mutex);
+                if(cat_wait_count>1){
+                        another_cat_eat = 1;
+                        V(cats_queue);
+                }
+                V(mutex);
+        }
+        kprintf("\nCat %d is in the kitchen", catNum);
+
+}
+
+static void CatDishes(int *myDish, int catNum){
+        P(dish_mutex);
+        if(!dish1_busy){
+                dish1_busy = 1;
+                myDish = 1;
+        } else {
+                assert(!dish2_busy);
+                dish2_busy = 1;
+                myDish = 2;
+        }
+        V(dish_mutex)
+        kprintf("\nCat %d is eating from dish %d", catNum, myDish);
+        clocksleep(1);
+        kprintf("\nCat %d is finished eating from dish %d", catNum, myDish);
+}
+
+static void releaseDishCat(int *myDish){
+        P(mutex);
+        P(dish_mutex);
+        if(myDish==1){
+                dish1_busy = 0;
+        } else { 
+                assert(myDish==2);
+                dish2_busy = 0;
+        }
+        V(dish_mutex);
+        cat_wait_count--;
+        V(mutex);
+}
+
+static void catsLeaving(int catNum){
+        if(first_cat_eat){
+                if(another_cat_eat){
+                        P(cat_done);
+                }
+                //print first cat leaving;
+                no_cat_eat = 1;
+                switchTurnsCat();
+        } else {
+                V(cat_done);
+        }
+        kprintf("\nCat %d is leaving the kitchen", catNum);
+}
+
+static void switchTurnsCat(){
+        P(mutex);
+        if(mice_wait_count>0){
+                V(mice_queue);
+        } else {
+                if(cat_wait_count>0){
+                        V(cats_queue);
+                } else {
+                        all_dishes_avail = 1;
+                }
+        }
+        V(mutex);
+}
+
+
+
+
+static void FirstMouseNoCat(int mouseNum){
+        P(mutex);
+        kprintf("\nMouse %d is hungry", mouseNum);
+        if(all_dishes_avail){
+                all_dishes_avail = 0;
+                V(mice_queue);
+        }
+        mice_wait_count++;
+        V(mutex);
+        P(mice_queue);
+        if(no_mouse_eat){
+                no_mouse_eat = 0;
+                first_mouse_eat = 1;
+        } else {
+                first_mouse_eat = 0;
+        }
+}
+
+static void FirstMouse(int mouseNum){
+
+        if(first_mouse_eat){
+                P(mutex);
+                if(mice_wait_count>1){
+                        another_mouse_eat = 1;
+                        V(mice_queue);
+                }
+                V(mutex);
+        }
+        kprintf("\nMouse %d is in the kitchen", mouseNum);
+
+}
+
+static void MouseDishes(int *myDish, int mouseNum){
+        P(dish_mutex);
+        if(!dish1_busy){
+                dish1_busy = 1;
+                myDish = 1;
+        } else {
+                assert(!dish2_busy);
+                dish2_busy = 1;
+                myDish = 2;
+        }
+        V(dish_mutex)
+        kprintf("\nMouse %d is eating from dish %d", mouseNum, myDish);
+        clocksleep(1);
+        kprintf("\nMouse %d is finished eating from dish %d", mouseNum, myDish);
+}
+
+static void releaseDishMouse(int *myDish){
+        P(mutex);
+        P(dish_mutex);
+        if(myDish==1){
+                dish1_busy = 0;
+        } else { 
+                assert(myDish==2);
+                dish2_busy = 0;
+        }
+        V(dish_mutex);
+        mice_wait_count--;
+        V(mutex);
+}
+
+static void mouseLeaving(int mouseNum){
+        if(first_mouse_eat){
+                if(another_mouse_eat){
+                        P(mouse_done);
+                }
+                no_mouse_eat = 1;
+                switchTurnsMouse();
+        } else {
+                V(mouse_done);
+        }
+        kprintf("\nMouse %d is leaving the kitchen", mouseNum);
+}
+
+static void switchTurnsMouse(){
+        P(mutex);
+        if(cats_wait_count>0){
+                V(cats_queue);
+        } else {
+                if(mice_wait_count>0){
+                        V(mice_queue);
+                } else {
+                        all_dishes_avail = 1;
+                }
+        }
+        V(mutex);
+}
 /*
  * catsem()
  *
@@ -78,6 +280,16 @@ catsem(void * unusedpointer,
 
         (void) unusedpointer;
         (void) catnumber;
+        int myDish = 0;
+
+        FirstCatNoMouse(catnumber);
+        FirstCat(catnumber);
+        CatDishes(&myDish, catnumber);
+        releaseDishCat(&myDish);
+        catsLeaving(catnumber);
+        switchTurnsCat();
+
+
 }
         
 
@@ -108,6 +320,18 @@ mousesem(void * unusedpointer,
 
         (void) unusedpointer;
         (void) mousenumber;
+
+        int myDish = 0;
+
+        FirstMouseNoCat(mousenumber);
+        FirstMouse(mousenumber);
+        MouseDishes(&myDish, mousenumber);
+        releaseDishMouse(&myDish);
+        MouseLeaving(mousenumber);
+        switchTurnsMouse();
+
+
+
 }
 
 
@@ -138,6 +362,8 @@ catmousesem(int nargs,
 
         (void) nargs;
         (void) args;
+
+        bowls = sem_create("bowls", 2);
    
         /*
          * Start NCATS catsem() threads.
